188855b5b39af4e9acd61a3aab8f5ec57ecfdfce
Natthapong Kanna <natthapong_ka65@live.rmutl.ac.th>
2024-06-10 12:46:15 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { useState } from 'react'
 import Navbar from './component/Navbar/Navbar'
 import Hero from './component/Hero/Hero'
 import About from './component/About/About'
-import Services from './component/Services/Services'
+
 import Mywork from './component/Mywork/Mywork'
 import Contact from './component/Contact/Contact'
-import Footer from './component/Footer/Footer'
+
 
 
 
